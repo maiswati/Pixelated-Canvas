696a3c51d4a4a9441c8f1cf13df4132933c2a885
maiswati <151076696+maiswati@users.noreply.github.com>
2025-04-11 18:42:20 +0530

    fix render deployment

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,6 +1,8 @@
+import {Router} from 'express';
 import { createOrder, paymentVerification } from "../controllers/transaction.controller.js";
 import { postShippingAddress } from "../controllers/transaction.controller.js";
 
+const router = Router();
 router.post('/createOrder/:paymentId', createOrder);
 router.post('/verify', paymentVerification )
 router.post('/submitShippingAddress/:paintingId/:buyerId', postShippingAddress);
