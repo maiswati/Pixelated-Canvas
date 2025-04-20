bbfabb00bdea5779af841d752a0c8410ecc3c5de
maiswati <151076696+maiswati@users.noreply.github.com>
2025-04-20 09:56:39 +0530

    Index.js Changed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://pixelated-canvas-frontend.vercel.app/',
+    origin: 'https://pixelated-canvas-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
   }));
